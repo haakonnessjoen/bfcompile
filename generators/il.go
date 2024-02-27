571338a6eefa9bf2e4e6b531ed837dd77e7533b6
Håkon Nessjøen <haakon.nessjoen@gmail.com>
2024-02-27 21:27:56 +0100

    first try, saving for reference

--- generators/il.go
+++ generators/il.go
@@ -3,6 +3,7 @@ package generators
 import (
 	l "bcomp/lexer"
 	"fmt"
+	"os"
 )
 
 // PrintIL prints the tokens as IL code
@@ -50,6 +51,67 @@ func PrintIL(tokens []ParseToken, includeComments bool) {
 		case l.JMPB:
 			fmt.Printf("	jmp @JMP%df\n", t.Extra)
 			fmt.Printf("@JMP%dbd\n", t.Extra)
+
+		case l.MUL:
+			// EXTRA = multiplier
+			// EXTRA2 = pointer
+
+			// p[%d] += *p * %d;
+			if t.Extra > 1 || t.Extra < -1 {
+				fmt.Printf("	%%v3 =w mul %%v, %d\n", t.Extra)
+
+				if t.Extra2 == 0 {
+					fmt.Printf("	%%v =w add %%v, %%v3\n")
+					fmt.Printf("	storeb %%v, %%p\n")
+					fmt.Printf("    %%v =w extub %%v\n")
+				} else {
+					if t.Extra2 > 0 {
+						fmt.Printf("	%%p2 =l add %%p, %d\n", t.Extra2)
+					} else if t.Extra2 < 0 {
+						fmt.Printf("	%%p2 =l sub %%p, %d\n", -t.Extra2)
+					}
+
+					fmt.Printf("	%%v2 =w loadub %%p2\n")
+					fmt.Printf("	%%v2 =w add %%v3, %%v2\n")
+					fmt.Printf("	storeb %%v2, %%p2\n")
+				}
+			} else if t.Extra == 1 || t.Extra == -1 {
+				// p[%d] += *p;
+				if t.Extra2 == 0 {
+					if t.Extra > 0 {
+						fmt.Printf("	%%v =w add %%v, %%v\n")
+					} else if t.Extra < 0 {
+						fmt.Printf("	%%v =w sub %%v, %%v\n")
+					}
+					fmt.Printf("	storeb %%v, %%p\n")
+					fmt.Printf("    %%v =w extub %%v\n")
+				} else {
+					if t.Extra2 > 0 {
+						fmt.Printf("	%%p2 =l add %%p, %d\n", t.Extra2)
+					} else if t.Extra2 < 0 {
+						fmt.Printf("	%%p2 =l sub %%p, %d\n", -t.Extra2)
+					}
+					fmt.Printf("	%%v2 =w loadub %%p2\n")
+					if t.Extra == 1 {
+						fmt.Printf("	%%v2 =w add %%v, %%v2\n")
+					} else if t.Extra == -1 {
+						fmt.Printf("	%%v2 =w sub %%v, %%v2\n")
+					}
+					fmt.Printf("	storeb %%v2, %%p2\n")
+				}
+			} else {
+				fmt.Fprintf(os.Stderr, "Internal error: MUL operation with wrong multiplier: %d\n", t.Extra)
+			}
+		case l.DIV:
+			// p[%d] /= %d;
+			if t.Extra2 == 0 {
+				fmt.Printf("	%%v =w div %%v, %d\n", t.Extra)
+				fmt.Printf("	storeb %%v, %%p\n")
+				fmt.Printf("    %%v =w extub %%v\n")
+			} else {
+				fmt.Fprint(os.Stderr, "Internal error: DIV operation with pointer other than 0 is not implemented\n")
+			}
+
 		}
 	}
 	fmt.Println("	ret 0")
